24414f8eeb01d966866bd86c07ee8e5555f9e153
Filip Dolinski <filipdolinski@gmail.com>
2018-06-29 15:05:25 +0200

    commit II

--- src/main/java/com/cs/app/controller/UserController.java
+++ src/main/java/com/cs/app/controller/UserController.java
@@ -11,20 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
-@RestController
+@RestController(value= "/users")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping("/user")
+    @GetMapping
     public List<User> getUsers() {
 
         return userService.getUsers();
 
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping(value="/user/{id}")
     public User getUser(@PathVariable int id){
         return userService.getUser(id);
     }
--- src/main/java/com/cs/app/service/UserService.java
+++ src/main/java/com/cs/app/service/UserService.java
@@ -2,25 +2,29 @@ package com.cs.app.service;
 
 import com.cs.app.dao.UserDAO;
 import com.cs.app.model.User;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
 
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     UserDAO userDAO;
 
     public List<User> getUsers() {
-
+        LOG.info("1234");
         return userDAO.getUsers();
     }
 
-    public User getUser(int id){
+    public User getUser(int id) {
+        LOG.warn("12 {} k12 {}", id, "45");
         return userDAO.getUser(id);
     }
 
